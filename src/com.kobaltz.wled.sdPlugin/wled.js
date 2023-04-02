7fab81dcc17ab3fa47cdfe936c56f2cf4a75ebe1
Dave Kimura <dave@k-innovations.net>
2023-04-02 15:44:44 -0400

    prep for action toggle icon

--- src/com.kobaltz.wled.sdPlugin/wled.js
+++ src/com.kobaltz.wled.sdPlugin/wled.js
@@ -3,26 +3,31 @@
 
 const togglePowerAction = new Action('com.kobaltz.wled.togglepower');
 
-$SD.onConnected(({ actionInfo, appInfo, connection, messageType, port, uuid }) => {
-});
+$SD.onConnected(({ actionInfo, appInfo, connection, messageType, port, uuid }) => {});
 
 togglePowerAction.onKeyUp(({ action, context, device, event, payload }) => {
   fetch(`http://${payload.settings.ip_address}/json/state`, {
     method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
     body: JSON.stringify({
-      "on": 't'
-    })
+      "on": 't',
+      "v": true
+    }).then(response => response.json())
+      .then(data => console.log(data))
   })
 });
 
 togglePowerAction.onDialRotate(({ action, context, device, event, payload }) => {
 });
 
+togglePowerAction.onWillAppear(({ action, context, device, event, payload }) => {
+  fetch(`http://${payload.settings.ip_address}/json/state`)
+});
+
 
 const setSceneAction = new Action('com.kobaltz.wled.setscene');
-
-$SD.onConnected(({ actionInfo, appInfo, connection, messageType, port, uuid }) => {
-});
 
 setSceneAction.onKeyUp(({ action, context, device, event, payload }) => {
   fetch(`http://${payload.settings.ip_address}/json/state`, {

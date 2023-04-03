7a8d41b2ad46bd97b61b45aafdf74b19450cf467
Dave Kimura <dave@k-innovations.net>
2023-04-03 01:26:22 -0400

    v1.0.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+# 2023-04-03 - Stream Deck + support
+  - Control the brightness of the lights via the dial
+
 # 2023-04-02 - First Release
   - Initial release for WLED plugin
   - Supports toggling power and selecting effects
--- src/com.kobaltz.wled.sdPlugin/wled.js
+++ src/com.kobaltz.wled.sdPlugin/wled.js
@@ -14,16 +14,78 @@ togglePowerAction.onKeyUp(({ action, context, device, event, payload }) => {
     body: JSON.stringify({
       "on": 't',
       "v": true
-    }).then(response => response.json())
-      .then(data => console.log(data))
+    })
   })
+  .then(response => response.json())
+  .then(data => console.log(data))
 });
 
 togglePowerAction.onDialRotate(({ action, context, device, event, payload }) => {
+  const value = 0;
+  fetch(`http://${payload.settings.ip_address}/json/state`)
+  .then(response => response.json())
+  .then(data => {
+    let value = Math.min(data.bri + (payload.ticks * 5), 255);
+    fetch(`http://${payload.settings.ip_address}/json/state`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        "bri": value
+      })
+
+    })
+    .then(response => response.json())
+    .then(data => {
+      $SD.setFeedback(context, {
+        'value': Math.round((value / 255) * 100),
+      })
+    })
+  })
+});
+
+togglePowerAction.onDialPress(({ action, context, device, event, payload }) => {
+  if (payload.pressed === false) {
+    fetch(`http://${payload.settings.ip_address}/json/state`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        "on": 't',
+        "v": true
+      })
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+  }
+});
+
+togglePowerAction.onTouchTap(({ action, context, device, event, payload }) => {
+  fetch(`http://${payload.settings.ip_address}/json/state`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      "on": 't',
+      "v": true
+    })
+  })
+  .then(response => response.json())
+  .then(data => console.log(data))
+  console.log(device)
 });
 
 togglePowerAction.onWillAppear(({ action, context, device, event, payload }) => {
   fetch(`http://${payload.settings.ip_address}/json/state`)
+  .then(response => response.json())
+  .then(data => {
+    $SD.setFeedback(context, {
+      'value': Math.round((data.bri / 255) * 100),
+    })
+  })
 });
 
 

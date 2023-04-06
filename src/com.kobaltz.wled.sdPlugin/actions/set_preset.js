7a45ee742ffd65764cccf6fe55117a20655c9071
Dave Kimura <dave@k-innovations.net>
2023-04-05 22:42:02 -0400

    - Added presets support   I'm not sure if it is possible to get the list of names, so   for the time being, it's just a list of the possible presets   to pick

---
+++ src/com.kobaltz.wled.sdPlugin/actions/set_preset.js
@@ -0,0 +1,14 @@
+const setPresetAction = new Action('com.kobaltz.wled.setpreset');
+
+setPresetAction.onKeyUp(({ action, context, device, event, payload }) => {
+  let wled_paylod = {
+    "on": true,
+    "v": true,
+    "ps": payload.settings.preset
+  }
+  fetch(`http://${payload.settings.ip_address}/json/state`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(wled_paylod)
+  })
+})
